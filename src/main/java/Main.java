46abc19415b1919dc71caab7147337c815601356
Bruno Vinicius da Silva Corsini <169068441+brunovsilv@users.noreply.github.com>
2024-09-02 12:13:41 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+  
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(Scanner.in);
+    
+    System.out.println("Ola, me diga seu nome completo: "); 
+    String nome completo = sc.next();
+    System.out.println("idade");
+    String idade = sc.next();
+    System.out.println("qual o seu sexo:");
+    Double sexo = 2;
+    System.out.println("O seu peso ideal e:");
+    if peso = sc.next.();
+    System.out.println("Obrigado por usar nosso aplicativo");
+    
+    sc.close();
+  }
+
+}
